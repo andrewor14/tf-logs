569fcaef89a4eff9b25d1205bdedc087f02dba6c
Andrew Or <andrewor14@gmail.com>
2019-05-05 17:12:31 -0400

    temp

--- plot.py
+++ plot.py
@@ -29,10 +29,34 @@ def plot_experiment(ax1, ax2, experiment_name, mode):
     variances[num_workers].append(variance)
   (avg_x_data, avg_y_data, avg_y_min, avg_y_max) = extract_plot_data(averages)
   (var_x_data, var_y_data, _, _) = extract_plot_data(variances)
+
+  # HACK ALERT (duplicate code)
+  averages2 = {} # num workers -> averages
+  variances2 = {} # num workers -> variances
+  experiment_name = "single-worker-comparison-8192"
+  experiment_dir = "data/%s" % experiment_name
+  for log_dir in os.listdir(experiment_dir):
+    num_workers = int(re.match(".*horovod.*_(\d+)workers", log_dir).groups()[0])
+    if num_workers not in averages2:
+      averages2[num_workers] = []
+    if num_workers not in variances2:
+      variances2[num_workers] = []
+    log_dir = os.path.join(experiment_dir, log_dir)
+    (average, variance) = parse_average_and_variance(experiment_name, mode, log_dir)
+    averages2[num_workers].append(average)
+    variances2[num_workers].append(variance)
+  (_, avg_y_data2, avg_y_min2, avg_y_max2) = extract_plot_data(averages2)
+
   ax1.set_xticks(avg_x_data)
-  line1 = ax1.errorbar(avg_x_data, avg_y_data, yerr=[avg_y_min, avg_y_max], fmt="-x", color="b", linewidth=2)
-  line2 = ax2.errorbar(var_x_data, var_y_data, fmt="--", color="g")
-  ax1.legend([line1, line2], ["average", "variance"], loc="lower right")
+  #line1 = ax1.errorbar(avg_x_data, avg_y_data, yerr=[avg_y_min, avg_y_max], fmt="-x", color="b", linewidth=2)
+  #line2 = ax1.errorbar(avg_x_data, avg_y_data2, yerr=[avg_y_min2, avg_y_max2], fmt="-x", color="g", linewidth=2)
+  import numpy as np
+  avg_y_data = (np.array(avg_y_data) - np.array(avg_y_data2)).tolist()
+  avg_y_min = (np.array(avg_y_min) - np.array(avg_y_min2)).tolist()
+  avg_y_max = (np.array(avg_y_max) - np.array(avg_y_max2)).tolist()
+  line3 = ax1.errorbar(avg_x_data, avg_y_data, yerr=[avg_y_min, avg_y_max], fmt="-x", color="r", linewidth=2)
+
+  #ax1.legend([line1, line2], ["synthetic-andrew-trivial-8192", "single-worker-comparison"], loc="best")
 
 # Parse the average and variance of the metric specified by mode
 # Return a 2-tuple of (average, variance)
@@ -68,9 +92,10 @@ def do_plot(experiment_name, mode):
   ax1 = fig.add_subplot(1, 1, 1)
   ax1.set_xlabel("num workers")
   ax1.set_ylabel("average (s)")
-  ax1.set_title("horovod %s time (%s)" % (mode, experiment_name))
-  ax2 = ax1.twinx()
-  ax2.set_ylabel("variance")
+  ax1.set_title("horovod communication time (difference)")
+  #ax2 = ax1.twinx()
+  #ax2.set_ylabel("difference (s)")
+  ax2 = None
   plot_experiment(ax1, ax2, experiment_name, mode)
   fig.set_tight_layout({"pad": 1.5})
   fig.savefig(out_file)
