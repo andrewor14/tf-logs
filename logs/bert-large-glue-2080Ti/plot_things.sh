e40514a8e6510972bdde9adcc13bbcd135de88c0
Andrew Or <andrewor14@gmail.com>
2020-09-03 02:21:25 -0400

    Add parse/plot scripts for BERT large (2080 Ti)

---
+++ logs/bert-large-glue-2080Ti/plot_things.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CURRENT_DIR="$(basename $PWD)"
+GLUE_TASKS="SST-2 QNLI CoLA"
+cd ../..
+
+for glue_task in $GLUE_TASKS; do
+  export TITLE="BERT (large) GLUE $glue_task on 1 GPU"
+  export DATA_FILE_PREFIX="logs/${CURRENT_DIR}/bert-glue-${glue_task}_"
+  OUTPUT_FILE_PREFIX="bert-large-glue-${glue_task}-2080Ti"
+  OUTPUT_FILE="${OUTPUT_FILE_PREFIX}.pdf" STEPS_PER_EPOCH="100"\
+    ./plot_accuracy.py "$DATA_FILE_PREFIX"*vn.txt
+  OUTPUT_FILE="${OUTPUT_FILE_PREFIX}-time.pdf" XLABEL="Time elapsed (s)"\
+    DATA_FILE_SUFFIX="-time.txt" ./plot_accuracy.py "$DATA_FILE_PREFIX"*time.txt
+done
+
